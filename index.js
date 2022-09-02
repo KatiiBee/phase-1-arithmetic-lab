42b2fee7a8a1c8a56bea2b5470b2d057c4879c0f
Katie Demetriff <kdemetriff@gmail.com>
2022-09-02 04:59:18 -0400

    commit tree 5d8a63527e103c8af0caa666ddcfdc0a61ee7b78

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+const num1 = 2
+const num2 = 31
+let multiply = num1 * num2
+
+let random = 1
+Math.random();
+
+const num3 = 48
+const num4 = 12
+let mod = num3 / num4
+
+let max =20
+Math.max()
